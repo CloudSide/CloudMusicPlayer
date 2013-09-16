37c76fec6873622c6dd4c3134c1cdd138201f3dc
Bruce Chen <662005@qq.com>
2013-09-16 10:38:48 +0800

    Initial Commit

---
+++ CloudMusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CloudMusicPlayer
+//
+//  Created by Bruce on 13-9-16.
+//  Copyright (c) 2013年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CloudMusicPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CloudMusicPlayer
+//
+//  Created by Bruce on 13-9-16.
+//  Copyright (c) 2013年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
